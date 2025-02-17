9677414104714eeb5b58ef740d0a38aa9a672e11
Rajarshi Tambuli <rajarshitambuli@gmail.com>
2025-02-17 20:09:34 +0530

    Duplicate number in an array

---
+++ CyclicSort/FindDuplicate.java
@@ -0,0 +1,34 @@
+package CyclicSort;
+
+public class FindDuplicate {
+
+    public static void main(String[] args) {
+        int[] arr = {3, 2, 4, 2, 1};
+        System.out.println("The duplicate number is: " + find(arr));
+    }
+
+    public static int find(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+
+            if (arr[i] != i + 1) {
+                int correct = arr[i] - 1;
+                if (arr[i] != arr[correct]) {
+                    swap(arr, i, correct);
+                } else {
+                    return arr[i];
+                }
+            } else {
+                i++;
+            }
+        }
+        return -1;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+}
