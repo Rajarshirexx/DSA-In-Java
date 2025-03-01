4ae8860b30b82a057726481bfcd90360eb6f2609
Rajarshi Tambuli <rajarshitambuli@gmail.com>
2025-03-01 19:42:39 +0530

    count zeroes

--- Recursion/CountZeroes.java
+++ Recursion/CountZeroes.java
@@ -2,10 +2,22 @@ package Recursion;
 
 public class CountZeroes {
     public static void main(String[] args) {
-        
+        System.out.println(count(300201));
     }
 
     static int count(int n) {
-        return 
+        return helper(n, 0);
+    }
+
+    private static int helper(int n, int c) {
+        if (n == 0) {
+            return c;
+        }
+
+        int rem = n % 10;
+        if (rem == 0) {
+            return helper(n/10, c+1);
+        }
+        return helper(n/10, c);
     }
 }
