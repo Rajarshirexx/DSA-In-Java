02a3387dd0a96d2d6f32e3fc7dd13c789fd69f2d
Rajarshi Tambuli <rajarshitambuli@gmail.com>
2025-03-02 12:11:50 +0530

    ChecK sorted array or not

---
+++ RecursionInArrays/SortedArray.java
@@ -0,0 +1,16 @@
+package RecursionInArrays;
+
+public class SortedArray {
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 7, 9, 12, 15};
+        System.out.println(sorted(arr, 0));
+    }
+    static boolean sorted(int[] arr, int index) {
+        // base condition
+        if (index == arr.length - 1){
+            return true;
+        }
+
+        return arr[index] < arr[index + 1] && sorted(arr, index + 1);
+    }
+}
