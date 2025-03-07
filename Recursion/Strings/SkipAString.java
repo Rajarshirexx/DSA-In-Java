cd2005c9b1e8270345cddac1d005f2911c56f8bf
Rajarshi Tambuli <rajarshitambuli@gmail.com>
2025-03-08 01:54:33 +0530

    Skip a string using recursion

---
+++ Recursion/Strings/SkipAString.java
@@ -0,0 +1,19 @@
+package Recursion.Strings;
+
+public class SkipAString {
+    public static void main(String[] args) {
+        System.out.println(skipString("pineapple"));
+    }
+
+    static String skipString(String up) {
+        if (up.isEmpty()) {
+            return "";
+        }
+
+        if (up.startsWith("apple")){
+            return skipString(up.substring(5));
+        } else {
+            return up.charAt(0) + skipString(up.substring(1));
+        }
+    }
+}
